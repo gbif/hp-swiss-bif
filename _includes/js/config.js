97770fa89f9e2777ecc5e604e880988b13d87b16
giannijacot <99412754+giannijacot@users.noreply.github.com>
2024-04-02 15:35:08 +0200

    Corrected errors with case in predicate keys

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -27,12 +27,12 @@ var siteConfig = {
       "predicates": [
         {
           "type": "equals",
-          "key": "COUNTRY",
+          "key": "country",
           "value": countryCode
         },
         {
           "type": "equals",
-          "key": "NETWORK_KEY",
+          "key": "networkKey",
           "value": "0b00b924-016b-4954-96a7-2d9264b5d0ba"
         }
       ]
