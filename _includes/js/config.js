23eef3c7ad3ce0b96b4e6282b0312a77c53fed54
giannijacot <99412754+giannijacot@users.noreply.github.com>
2024-04-04 17:09:37 +0200

    Test "not" predicate

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -34,6 +34,14 @@ var siteConfig = {
           "type": "equals",
           "key": "networkKey",
           "value": "0b00b924-016b-4954-96a7-2d9264b5d0ba"
+        },
+        {
+        "type": "not",
+        "predicate": {
+            "type": "equals",
+            "key": "basisOfRecord ",
+            "value": "PreservedSpecimen"
+          }
         }
       ]
     },
